191619faa61c29ff3d3e56539091665c2b2565fc
rabih-dev <tabatchnikrabih@gmail.com>
2022-11-23 11:59:44 -0300

    finalizado lista de itens

--- Projetos/Lista de Itens/main.js
+++ Projetos/Lista de Itens/main.js
@@ -5,14 +5,47 @@ const itemList = document.querySelector('#items');
 //#endregion variables
 //#region functions
 function addItem(event){
-event.preventDefault();
-alert('demonio');
+    event.preventDefault();
+
+    let itemInput = document.getElementById('item')
+    let newItem = itemInput.value;
+    document.getElementById('item').value = ' ';
+
+    let li = document.createElement('li');
+    li.className = 'list-group-item';
+    li.appendChild(document.createTextNode(newItem));   
+    let deleteBtn = document.createElement('button');
+    deleteBtn.className = 'btn btn-danger  btn-sm float-end delete';
+    deleteBtn.appendChild(
+        document.createTextNode('X')
+    )
+    li.appendChild(deleteBtn);
+    itemList.appendChild(li);
+    
 }
 
-function searchItem(){
+function deleteItem(clickLocation){
+    if(clickLocation.target.classList.contains('delete')){
+        let li = clickLocation.target.parentElement;
+        itemList.removeChild(li);
+    }
+}
 
+function searchItem(event){
+    let txt = event.target.value.toLowerCase();
+    let allItems = itemList.getElementsByTagName('li');
+
+    Array.from(allItems).forEach(element => {
+        if(element.textContent.toLowerCase().indexOf(txt) != -1){
+            element.style.display = 'block'
+        }
+        else{
+            element.style.display = 'none'
+        }
+    });
 }
 //#endregion functions
 
 filter.addEventListener('keyup',searchItem);
 addForm.addEventListener('submit',addItem);
+itemList.addEventListener('click',deleteItem)
